83a6d53094e97921ca0ff688f140b29eb2e7600e
maxgaj <maxime.gaj@gmail.com>
2019-11-27 15:46:47 +0100

    angular codelab00

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fromEvent} from 'rxjs';
+import {map, mergeMapTo, takeUntil, tap} from 'rxjs/operators';
+import {log} from 'util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
+
+  ngOnInit(): void {
+    const mouseDrag$ = fromEvent<MouseEvent>(document, 'mousedown').pipe(
+      mergeMapTo(
+        fromEvent<MouseEvent>(document, 'mousemove').pipe(
+          takeUntil(fromEvent<MouseEvent>(document, 'mouseup'))
+        )
+      )
+    );
+
+    mouseDrag$.pipe(
+        tap(event => console.log(event)),
+        map((event) => `Position = ${event.clientX}px on the X axis and ${event.clientY}px on the Y axis`)
+      )
+      .subscribe(event => console.log(event));
+  }
 }
